7381f840c2d3db986fb0c747ddeb41ce34b65dd5
xiaobei <legend0508@163.com>
2020-07-03 08:28:58 +0800

    线程测试

--- juc-demo/src/main/java/com/xiaobei/java/demo/thread/CallableDemo.java
+++ juc-demo/src/main/java/com/xiaobei/java/demo/thread/CallableDemo.java
@@ -1,6 +1,10 @@
 package com.xiaobei.java.demo.thread;
 
+import org.junit.Test;
+
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 /**
  * Java多线程创建方式三：实现Callable接口
@@ -11,21 +15,40 @@ public class CallableDemo implements Callable<String> {
 
     @Override
     public String call() throws Exception {
+        return threadBody();
+    }
+
+    private String threadBody() {
         System.out.println(Thread.currentThread().getName() + "正在行军~~~");
         System.out.println(Thread.currentThread().getName() + "遭遇敌军~~~");
         System.out.println(Thread.currentThread().getName() + "奋勇杀敌！！！！");
         return "战斗胜利，俘虏敌军50000人";
     }
 
-    public static void usage1() {
-
+    /**
+     * 利用Callable接口的实现创建子线程类
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void usage1() throws ExecutionException, InterruptedException {
+        FutureTask<String> ft = new FutureTask<>(new CallableDemo());
+        new Thread(ft, "usage1").start();
+        String result = ft.get();
+        System.out.println(result);
     }
 
-    public static void usage2() {
-
+    /**
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void usage2() throws ExecutionException, InterruptedException {
+        Callable<String> c1 = this::threadBody;
+        FutureTask<String> ft = new FutureTask<>(c1);
+        new Thread(ft, "usage2").start();
+        System.out.println(ft.get());
     }
 
-    public static void main(String[] args) {
-
-    }
 }
