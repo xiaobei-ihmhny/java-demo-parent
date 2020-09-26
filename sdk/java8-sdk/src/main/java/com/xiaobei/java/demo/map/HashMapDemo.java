85b8d70e9d6dc4d66297b1637087a54462f1166a
xiaobei <legend0508@163.com>
2020-09-26 20:34:36 +0800

    jdk-HashMap扩容学习

---
+++ sdk/java8-sdk/src/main/java/com/xiaobei/java/demo/map/HashMapDemo.java
@@ -0,0 +1,121 @@
+package com.xiaobei.java.demo.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020/9/25 20:13
+ */
+public class HashMapDemo {
+
+    /**
+     * <h2>当存放第一个元素时</h2>
+     * // 新的容量大小
+     * newCap = DEFAULT_INITIAL_CAPACITY;
+     * // 新的阈值（达到该值将进行扩容）
+     * newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+     * // 将阈值保存到实例变量 threshold 中
+     * threshold = newThr;
+     * // 创建底层数组
+     * Node<K,V>[] newTab = (Node<K,V>[])new Node[newCap];
+     * // 将底层数组 table 指向新创建的数组
+     * table = newTab;
+     * // 返回新的数组
+     * return newTab;
+     *
+     * 当存放第13个元素时再次进入 {@link }
+     */
+    @Test
+    public void put() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "中");
+        map.put("2", "华");
+        map.put("3", "人");
+        map.put("4", "民");
+        map.put("5", "共");
+        map.put("6", "和");
+        map.put("7", "国");
+        map.put("8", "万");
+        map.put("9", "岁");
+        map.put("10", "共");
+        map.put("11", "产");
+        map.put("12", "党");
+        map.put("13", "万");
+        map.put("14", "岁");
+        System.out.println(map);
+    }
+
+    /**
+     * 测试 hash 冲突
+     *
+     */
+    @Test
+    public void testHashImpact() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");//1 -> 1 + 1 << 4 = 17
+        map.put("12", "12");//1 -> 1
+        //=====================
+        map.put("2", "2");// 2 -> 2 + 1 << 4 = 18
+        map.put("13", "13");// 2 -> 2
+        map.put("101", "101");// 2 -> 2 + 1 << 4 = 18
+        map.put("112", "112");// 2 -> 2 + 1 << 4 = 18
+        map.put("24", "24");// 2 -> 2
+        //=====================
+        map.put("3", "3");// 3 -> 3 + 1 << 4 = 20
+        map.put("14", "14");// 5 -> 5 + 1 << 4 = 22
+        //=====================
+        map.put("5", "5");// 5 -> 4 + 1 << 4 = 21
+        map.put("16", "16");// 5 -> 5
+        //=====================
+        map.put("7", "7");// 7 -> 4 + 1 << 4 = 23
+        map.put("8", "8");// 8 -> 4 + 1 << 4 = 24
+        System.out.println(map);
+    }
+
+    static class HashTestDomain {
+        private Long id;
+
+        private String name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public HashTestDomain setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public HashTestDomain setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof HashTestDomain)) return false;
+            HashTestDomain that = (HashTestDomain) o;
+            return Objects.equals(id, that.id) &&
+                    Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return (int)(id % 10);
+        }
+
+        @Override
+        public String toString() {
+            return "[id=" + id + ", name='" + name + "]";
+        }
+    }
+}
---
+++ sdk/java8-sdk/src/test/java/com/xiaobei/java/demo/map/HashMapTest.java
@@ -0,0 +1,118 @@
+package com.xiaobei.java.demo.map;
+
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+/**
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020/9/25 20:13
+ */
+public class HashMapTest {
+
+    /**
+     * <h2>运行结果：</h2>
+     * 1
+     * 16
+     * 128
+     * 1024
+     * 16384
+     *
+     * 结果为比当前指定数字大的最小的2的n次幂
+     */
+    @Test
+    public void tableSizeForTest() {
+        System.out.println(tableSizeFor(1));
+        System.out.println(tableSizeFor(10));
+        System.out.println(tableSizeFor(100));
+        System.out.println(tableSizeFor(1000));
+        System.out.println(tableSizeFor(10000));
+    }
+    /**
+     * The maximum capacity, used if a higher value is implicitly specified
+     * by either of the constructors with arguments.
+     * MUST be a power of two <= 1<<30.
+     */
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    @Test
+    public void testHashCode() {
+        isEquals("1", 9);
+        isEquals("12", 9);
+        isEquals("11", 9);
+    }
+
+    public void isEquals(String key, int n) {
+        System.out.printf("hash(key) : %s, tableSizeFor(n) : %d, index : %d, newIndex: %d\n",
+                Integer.toBinaryString(hash(key)), tableSizeFor(n), hash(key) & (tableSizeFor(n) - 1), hash(key) & (tableSizeFor(n)*2 - 1));
+    }
+
+    public int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    @Test
+    public void testStringHashCode() {
+        for (int i = 1; i < 10000; i++) {
+            if((String.valueOf(i).hashCode() & 15) == 2) {
+                System.out.println("map.put(\"" + i + "\", \"" + i + "\");");
+            }
+        }
+    }
+
+    @Test
+    public void testResize() {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("2", "2");// 2 + 1 << 4 = 18
+        map.put("13", "13");// 2
+        map.put("101", "101");// 2 + 1 << 4 = 18
+        map.put("112", "112");// 2 + 1 << 4 = 18
+        map.put("24", "24");// 2
+        map.put("123", "123");// 2 + 1 << 4 = 18
+        map.put("35", "35");// 2
+        Map<String, Integer> oldDist = new HashMap<>(8);
+        Map<String, Integer> newDist = new LinkedHashMap<>(128);
+        for(Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            oldDist.put(key, (String.valueOf(key).hashCode() & (1 << 4) - 1));
+            int result;
+            if((result = (String.valueOf(key).hashCode() & (1 << 5) - 1)) != 2) {
+                newDist.put(key, result);
+            }
+        }
+        for(Map.Entry<String, Integer> entry : oldDist.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if(value != 2) {
+                System.out.println("oldDist：" + key + " - " + value);
+            }
+        }
+        Set<Map.Entry<String, Integer>> entries = newDist.entrySet();
+        Iterator<Map.Entry<String, Integer>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("newDist：" + key + " - " + value);
+        }
+//        map.forEach((key, value) -> {
+//            oldDist.put(key, (String.valueOf(key).hashCode() & (1 << 4) - 1));
+//            newDist.put(key, (String.valueOf(key).hashCode() & (1 << 5) - 1));
+//        });
+//        Stream.of(oldDist).forEach(System.out::println);
+//        Stream.of(newDist).forEach(System.out::println);
+    }
+
+}
