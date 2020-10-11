2b89d938daf6eed9cc85325febc1a0d287c5516e
xiaobei <legend0508@163.com>
2020-10-12 07:52:29 +0800

    线程安全测试 学习

--- jvm/src/main/java/org/fenixsoft/jvm/chapter13/VectorTestCase_1.java
+++ jvm/src/main/java/org/fenixsoft/jvm/chapter13/VectorTestCase_1.java
@@ -9,6 +9,14 @@ public class VectorTestCase_1 {
 
     private static Vector<Integer> vector = new Vector<Integer>();
 
+    /**
+     * 运行过程中会出现异常：
+     * Exception in thread "Thread-2263" java.lang.ArrayIndexOutOfBoundsException: Array index out of range: 6
+     * 	at java.util.Vector.get(Vector.java:748)
+     * 	at org.fenixsoft.jvm.chapter13.VectorTestCase_1$2.run(VectorTestCase_1.java:31)
+     * 	at java.lang.Thread.run(Thread.java:745)
+     * @param args
+     */
     public static void main(String[] args) {
         while (true) {
             for (int i = 0; i < 10; i++) {
