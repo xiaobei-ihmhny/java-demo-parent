eeea948470a787bf22d44a1104a59b15c22f19e3
zhibeibei <Z471200a>
2020-11-20 17:36:19 +0800

    redis sentinel spring配置

--- redis-demo/src/main/java/com/xiaobei/middleware/redis/sentinel/SentinelMain.java
+++ redis-demo/src/main/java/com/xiaobei/middleware/redis/sentinel/SentinelMain.java
@@ -2,31 +2,42 @@ package com.xiaobei.middleware.redis.sentinel;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
  * @date 2020/11/20 6:45
  */
-@Component
+@Configuration
 @PropertySource(value = "classpath:/META-INF/redis-sentinel.properties")
 public class SentinelMain {
 
-    @Value("${spring.redis.sentinel.master}")
-    private String name;
-
-    @Value("${spring.redis.sentinel.nodes}")
-    private String sentinels;
+    @Bean
+    public JedisSentinelPool pool(
+            @Value("${spring.redis.sentinel.master}") String name,
+            @Value("${spring.redis.sentinel.nodes}") String[] sentinels) {
+        return new JedisSentinelPool(name,
+                Arrays.stream(sentinels).collect(Collectors.toSet()));
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(SentinelMain.class);
         applicationContext.refresh();
-        SentinelMain sentinelMain = applicationContext.getBean(SentinelMain.class);
-        System.out.println(sentinelMain.name);
-        System.out.println(sentinelMain.sentinels);
+        JedisSentinelPool pool = applicationContext.getBean(JedisSentinelPool.class);
+        Jedis jedis = pool.getResource();
+        jedis.set("xiaobei", "100");
+        System.out.println(jedis.get("xiaobei"));
         applicationContext.close();
     }
 }
