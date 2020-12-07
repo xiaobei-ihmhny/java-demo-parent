7e7f9b418b0327f950961ea2a2832c6a8a7c1909
zhibeibei <Z471200a>
2020-12-07 15:47:32 +0800

    itext测试

--- poi-demo/itextpdf-demo/src/test/java/com/xiaobei/itextpdf/demo/ItextPdfTest.java
+++ poi-demo/itextpdf-demo/src/test/java/com/xiaobei/itextpdf/demo/ItextPdfTest.java
@@ -1,8 +1,15 @@
 package com.xiaobei.itextpdf.demo;
 
+import org.junit.Test;
+
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
  * @date 2020-12-04 16:32:32
  */
 public class ItextPdfTest {
+
+    @Test
+    public void test() {
+
+    }
 }
