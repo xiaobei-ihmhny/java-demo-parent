f1a627d1ea9525874d2c49e2107e3eea28b8899b
xiaobei <legend0508@163.com>
2020-12-25 17:52:17 +0800

    配合 RestTemplate 添加Controller

--- springmvc-no-webxml-demo/src/main/java/com/xiaobei/java/demo/customize/autoconfig/controller/HelloWorldController.java
+++ springmvc-no-webxml-demo/src/main/java/com/xiaobei/java/demo/customize/autoconfig/controller/HelloWorldController.java
@@ -1,8 +1,15 @@
 package com.xiaobei.java.demo.customize.autoconfig.controller;
 
+import com.xiaobei.java.demo.customize.autoconfig.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
@@ -11,9 +18,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
+    private static final Logger LOGGER = Logger.getLogger(HelloWorldController.class.getName());
+
+    private final Map<Long, User> userMap = new HashMap<>(16);
+
     @RequestMapping
     @ResponseBody
     public String helloWorld() {
         return "hello world";
+    }
+
+    @RequestMapping(value = "/path/{param1}/{param2}",method = RequestMethod.GET)
+    @ResponseBody
+    public String mapParams(@PathVariable("param1") String param1,
+                            @PathVariable("param2") String param2,
+                            HttpServletRequest request) {
+        return request.getRequestURL().toString();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/header",method = RequestMethod.GET, headers = {"password=xiaohui"})
+    public String customHeaders(String name, HttpServletResponse response) {
+        response.addHeader("answer", "beibei");
+        return "this is header request, your header consider the current header named password and value xiaohui: " + name;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/save",method = RequestMethod.POST)
+    public String postForObject(@RequestBody User user) {
+        LOGGER.log(Level.INFO, "用户信息为：" + user);
+        userMap.put(user.getId(), user);
+        return "user信息保存成功";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/{userId}",method = RequestMethod.GET)
+    public User getForObject(@PathVariable("userId") Long userId) {
+        LOGGER.log(Level.INFO, "用户信息为：" + userId);
+        return userMap.get(userId);
     }
 }
