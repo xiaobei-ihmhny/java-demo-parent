1233a2433f625c06e09fea14b0cd0cecf1ecaaf3
xiaobei <legend0508@163.com>
2021-01-05 07:53:08 +0800

    dubbo配置升级 示例

--- dubbo-demo-parent/dubbo-spring-boot-provider-demo/src/main/java/com/xiaobei/dubbo/springboot/demo/DubboSpringBootDemoApplication.java
+++ dubbo-demo-parent/dubbo-spring-boot-provider-demo/src/main/java/com/xiaobei/dubbo/springboot/demo/DubboSpringBootDemoApplication.java
@@ -1,24 +1,14 @@
 package com.xiaobei.dubbo.springboot.demo;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
-public class DubboSpringBootDemoApplication implements ApplicationContextAware {
+public class DubboSpringBootDemoApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DubboSpringBootDemoApplication.class, args);
 	}
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		Object obj1 = applicationContext.getBean("payServiceImpl");
-		Object obj2 = applicationContext.getBean("ServiceBean:com.xiaobei.dubbo.api.IPayService");
-		System.out.println(111);
-	}
 }
