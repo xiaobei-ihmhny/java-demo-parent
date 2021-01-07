e77e0d6840e855484ef284a20d708da1b19a1122
xiaobei <legend0508@163.com>
2021-01-08 07:27:52 +0800

    jvm class结构

---
+++ jvm/src/main/java/org/fenixsoft/clazz/TestClass.java
@@ -0,0 +1,16 @@
+package org.fenixsoft.clazz;
+
+/**
+ * TODO
+ *
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2021-01-08 06:48:48
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
