ed628dda3ef7c6708928a9ee4bc7c2475a5e6570
xiaobei <legend0508@163.com>
2021-04-22 20:08:39 +0800

    mybatis-plus demo

---
+++ code-generator-parent/mybatis-plus-generator-demo/src/main/java/com/xiaobei/code/generator/mybatisplus/config/GlobalConfig.java
@@ -0,0 +1,10 @@
+package com.xiaobei.code.generator.mybatisplus.config;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:legend0508@163.com">xiaobei-ihmhny</a>
+ * @date 2021-04-22 18:44:44
+ */
+public class GlobalConfig {
+}
